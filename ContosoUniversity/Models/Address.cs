a9651a88cfd039f87365b04dbd23e285eb211e1a
bisratz <35151845+bisratz@users.noreply.github.com>
2018-03-03 22:19:22 -0800

    MTcode

---
+++ ContosoUniversity/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Email Address")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@contoso.edu")]
+        public string Email { get; set; }
+    }
+}
